7a5e70081a22a6e7ede44d7962212933202f9bcb
BuildTools <unconfigured@null.spigotmc.org>
2022-09-13 22:15:09 -0400

    added compiled pkg to commit

--- www/index.js
+++ www/index.js
@@ -1,13 +1,33 @@
 import { Universe } from "rust-wasm";
 
 const pre = document.getElementById("game-of-life-canvas");
-console.log(Universe);
-const universe = Universe.new();
+const pauseButton = document.getElementById("pause");
+const resetButton = document.getElementById("reset");
+const speedSlider = document.getElementById("speed");
+let paused = true;
+console.log(resetButton);
+let universe = Universe.new();
+
+pauseButton.onclick = () => {
+	paused = !paused;
+	pauseButton.innerText = paused ? "Unpause" : "Pause";
+};
+resetButton.onclick = () => {
+	//console.log("hi");
+	universe = Universe.new();
+	pre.innerHTML = universe.render();
+	paused = true;
+	pauseButton.innerText = paused ? "Unpause" : "Pause";
+};
+pre.innerHTML = universe.render();
 
 const renderLoop = () => {
-	console.log(universe);
-	pre.textContent = universe.render();
-	universe.tick();
+	if (!paused) {
+		pre.innerHTML = universe.render();
+		for (let i = 0; i < speedSlider.value; i++) {
+			universe.tick();
+		}
+	}
 
 	requestAnimationFrame(renderLoop);
 };
